38bce057b00f98b52b5a364c0ceea55099840fc6
Reorx <novoreorx@gmail.com>
2013-12-17 15:09:09 +0800

    rewrite cursor

--- simplemongo/__init__.py
+++ simplemongo/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__all__ = (
+__all__ = [
     'Document', 'Struct', 'ObjectId', 'oid',
-)
+]
 
 from bson.objectid import ObjectId
-from .dstruct import Struct, StructuredDict, StructDefineError
+from .dstruct import Struct
 from .models import Document, oid
--- simplemongo/cursor.py
+++ simplemongo/cursor.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pymongo.cursor import Cursor as PymongoCursor
+from pymongo.cursor import Cursor
 
 
-class Cursor(PymongoCursor):
+class SimplemongoCursor(Cursor):
     def __init__(self, *args, **kwargs):
-        self.__wrap = None
-        if kwargs:
-            self.__wrap = kwargs.pop('wrap', None)
-        super(Cursor, self).__init__(*args, **kwargs)
+        self.__wrapper = kwargs.pop('wrapper', None)
+
+        super(SimplemongoCursor, self).__init__(*args, **kwargs)
 
     def next(self):
-        if self._Cursor__empty:
-            raise StopIteration
-        db = self._Cursor__collection.database
-        if len(self.__data) or self._refresh():
-            if self._Cursor__manipulate:
-                d = db._fix_outgoing(self._Cursor__data.popleft(), self._Cursor__collection)
-            else:
-                d = self._Cursor__data.popleft()
-            if self.__wrap:
-                return self.__wrap(d, from_db=True)
-            else:
-                return d
+        ## Original `next` method:
+        # if self._Cursor__empty:
+        #     raise StopIteration
+        # db = self._Cursor__collection.database
+        # if len(self._Cursor__data) or self._refresh():
+        #     if self._Cursor__manipulate:
+        #         raw = db._fix_outgoing(self._Cursor__data.popleft(),
+        #                                 self._Cursor__collection)
+        #     else:
+        #         raw = self._Cursor__data.popleft()
+
+        # Directly call pymongo Cursor's `next` method
+        raw = super(SimplemongoCursor, self).next()
+
+        if self.__wrapper:
+            return self.__wrapper(raw, from_db=True)
         else:
-            raise StopIteration
+            return raw
 
     def __getitem__(self, index):
-        rv = super(Cursor, self).__getitem__(index)
-        if self.__wrap and isinstance(rv, dict):
-            return self.__wrap(rv)
-        return rv
+        rv = super(SimplemongoCursor, self).__getitem__(index)
+
+        # `rv` could be `self` or document dict
+        if self.__wrapper and isinstance(rv, dict):
+            return self.__wrapper(rv, from_db=True)
+        else:
+            return rv
